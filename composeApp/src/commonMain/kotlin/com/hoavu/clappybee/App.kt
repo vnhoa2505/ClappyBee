82f6e2b7c2bd316adb5d15e7089ae853ef180c93
Hoà Vũ <hoavu@MacBook-Air-cua-Hoa.local>
2025-01-12 19:17:19 +0700

    init project

---
+++ composeApp/src/commonMain/kotlin/com/hoavu/clappybee/App.kt
@@ -0,0 +1,13 @@
+package com.hoavu.clappybee
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/hoavu/clappybee/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.hoavu.clappybee
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
