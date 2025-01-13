d6c2e5b4f8a297329ed8f00a81d54d704ef7d89f
Hoà Vũ <hoavu@MacBook-Air-cua-Hoa.local>
2025-01-13 12:23:04 +0700

    add game background, jump and fall logic

--- composeApp/src/commonMain/kotlin/com/hoavu/clappybee/App.kt
+++ composeApp/src/commonMain/kotlin/com/hoavu/clappybee/App.kt
@@ -1,13 +1,130 @@
 package com.hoavu.clappybee
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import clappybee.composeapp.generated.resources.Res
+import clappybee.composeapp.generated.resources.background
+import com.hoavu.clappybee.domain.Game
+import com.hoavu.clappybee.domain.GameStatus
+import com.hoavu.clappybee.util.ChewyFontFamily
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        var screenWidth by remember { mutableStateOf(0) }
+        var screenHeight by remember { mutableStateOf(0) }
+        var game by remember { mutableStateOf(Game()) }
 
+        LaunchedEffect(Unit) {
+            game.start()
+        }
+
+        LaunchedEffect(game.status) {
+            while (game.status == GameStatus.Started) {
+                withFrameMillis {
+                    game.updateGameProgress()
+                }
+            }
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(Res.drawable.background),
+                contentDescription = "Background image",
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .onGloballyPositioned {
+                    val size = it.size
+                    if (screenWidth != size.width || screenHeight != size.height) {
+                        screenWidth = size.width
+                        screenHeight = size.height
+                        game = game.copy(
+                            screenWidth = size.width,
+                            screenHeight = size.height
+                        )
+                    }
+                }
+                .clickable {
+                    if (game.status == GameStatus.Started) {
+                        game.jump()
+                    }
+                }
+        ) {
+            drawCircle(
+                color = Color.Blue,
+                radius = game.bee.radius,
+                center = Offset(
+                    x = game.bee.x,
+                    y = game.bee.y
+                )
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 48.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "BEST: 0",
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                fontFamily = ChewyFontFamily()
+            )
+            Text(
+                text = "0",
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                fontFamily = ChewyFontFamily()
+            )
+        }
+
+        if (game.status == GameStatus.Over) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Black.copy(alpha = 0.5f)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Game Over!",
+                    color = Color.White,
+                    fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = ChewyFontFamily()
+                )
+            }
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/hoavu/clappybee/domain/Bee.kt
@@ -0,0 +1,7 @@
+package com.hoavu.clappybee.domain
+
+data class Bee(
+    val x: Float,
+    val y: Float,
+    val radius: Float = 30f,
+)
