33d42b4fde73a62d74275d6d78bae3e71591b7fc
Hoà Vũ <hoavu@MacBook-Air-cua-Hoa.local>
2025-01-19 11:46:54 +0700

    implement audioplayer for ios target

--- composeApp/src/commonMain/kotlin/com/hoavu/clappybee/domain/Game.kt
+++ composeApp/src/commonMain/kotlin/com/hoavu/clappybee/domain/Game.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.hoavu.clappybee.util.Platform
 import com.russhwolf.settings.ObservableSettings
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -12,15 +13,16 @@ import kotlin.random.Random
 const val SCORE_KEY = "SCORE"
 
 data class Game(
+    val platform: Platform,
     val screenWidth: Int = 0,
     val screenHeight: Int = 0,
     val gravity: Float = 0.7f,
     val beeRadius: Float = 30f,
     val beeJumpImpulse: Float = -12f,
-    val beeMaxVelocity: Float = 25f,
+    val beeMaxVelocity: Float = if (platform == Platform.Android) 25f else 10f,
     val pipeWidth: Float = 150f,
-    val pipeVelocity: Float = 5f,
-    val pipeGapSize: Float = 250f,
+    val pipeVelocity: Float = if (platform == Platform.Android) 5f else 2.5f,
+    val pipeGapSize: Float = if (platform == Platform.Android) 250f else 300f,
 ): KoinComponent {
     private val settings: ObservableSettings by inject()
     private val audioPlayer: AudioPlayer by inject()
